d265e36676c0358bd9380969371b202fd550d0c1
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2024-11-19 15:07:12 -0800

    upload form has css now;

--- client/src/screens/uploadProduct.js
+++ client/src/screens/uploadProduct.js
@@ -2,39 +2,59 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../stylesheets/App.css'
 
+// update css
+// connect to db next 
+    // post products according to schema
 const UploadProduct = () => {
     return (
-        <div>
+        <div id='pcontainer'>
             <h1>This is the upload product page</h1>
             <form id="uploadproduct">
-                <div id="namediv">
+                <div id="pdiv">
                     <p>Product name:</p>
-                    <input type="text" id="pname"></input>
+                    <input 
+                        type="text" 
+                        id="pname"
+                        className="prod-input" 
+                        placeholder="Enter product name..."
+                    />
                 </div>
-                <div id="typediv">
+                <div id="pdiv">
                     <p>Type of product:</p>
-                    <select class="form-select" aria-label='Choose the type of beauty product'>
-                        <option selected>Type of Product:</option>
+                    <select 
+                        className="prod-input"  
+                        aria-label='Choose the type of beauty product'
+                        defaultValue="none"
+                    >
+                        <option value='none'>Select the type of product:</option>
                         <option value="Skin Care">Skin Care</option>
                         <option value="Hair Care">Hair Care</option>
                         <option value="Not Listed">Not Listed</option>
                     </select>
                 </div>
-                <div id="pricediv">
+                <div id="pdiv">
                     <p>Price Range:</p>
-                    <select class="form-select" aria-label='Choose the price range'>
-                        <option selected></option>
+                    <select 
+                        className="prod-input" 
+                        aria-label='Choose the price range'
+                        defaultValue="none"
+                    >
+                        <option value='none'>Select the price range:</option>
                         <option value="$">$</option>
                         <option value="$$">$$</option>
                         <option value="$$$">$$$</option>
                     </select>
                 </div>
-                <div id="imagediv">
+                <div id="pdiv">
                     <p>Image hyperlink:</p>
-                    <input type="url"></input>
+                    <input className="prod-input" type="url" placeholder='Enter an image URL...'></input>
                 </div>
-                <input type='submit' value='Submit'></input>
-                <input type="reset" value="Reset"></input>
+                <div id='buttondiv'>
+                    <input id='pbutton' type='submit' value='Submit'></input>
+                </div>
+                <div id='buttondiv'>
+                    <input id='pbutton' type="reset" value="Reset"></input>
+                </div>
             </form>
         </div>
     );
