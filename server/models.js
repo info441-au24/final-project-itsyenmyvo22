af89feb2adaa0aab040feac455b7f594f04a2f65
Yenmy Vo <114707371+itsyenmyvo22@users.noreply.github.com>
2024-11-20 11:31:08 -0800

    create schemas

--- server/models.js
+++ server/models.js
@@ -11,8 +11,34 @@ async function connectDatabase() {
       name: String,
       category: String,
       price: String,
+      url: String
     });
     models.Post = mongoose.model('Post', postSchema);
+
+    const collectionSchema = new mongoose.Schema({
+      username: String,
+      collection_name: String,
+      products: [String],
+      collection_description: String
+    });
+    models.Collection = mongoose.model('Collection', collectionSchema);
+
+    const reviewSchema = new mongoose.Schema({
+      username: String,
+      review: String,
+      productID: String,
+      rating: Number,
+      comments: [String],
+      created_date: Date
+    });
+    models.Review = mongoose.model('Review', reviewSchema);
+
+    const commentSchema = new mongoose.Schema({
+      username: String,
+      comment: String,
+      created_date: Date
+    });
+    models.Comment = mongoose.model('Comment', commentSchema);
 
     console.log('Mongoose models created');
   } catch (error) {
