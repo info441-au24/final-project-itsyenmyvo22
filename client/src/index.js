9b6cd542e970cb2ac3954f46f464b4c2eedf39d4
Celestine Le <cmle@uw.edu>
2024-12-05 14:34:48 -0800

    fixed index.css import

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './stylesheets/index.css';
+import './assets/stylesheets/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
